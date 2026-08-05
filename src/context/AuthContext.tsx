05462fbf33ddb7f08607bc3a144e2e90a50a9cc7
Carlos Alvarez <carlosalvarez214@gmail.com>
2026-08-05 16:04:49 -0400

    feat: implement AuthContext for Firebase authentication management

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,31 @@
+import { createContext, useState, useEffect, useContext, type ReactNode } from "react";
+import { onAuthStateChanged, type User } from "firebase/auth";
+
+interface AuthContextType {
+    user: null | User,
+    setUser: (user:User) => void;
+}
+const AuthContext = createContext<AuthContextType>({
+    user: null,
+    setUser: (user:User) => {}
+})
+
+export const AuthProvider: React.FC<{children: ReactNode}> = ({children}) => {
+    const [user, setUser] = useState<User | null>(null);
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser(null);
+            }
+        });
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{user, setUser}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
